e0154074390b1adc88098a2b38ecdbd62e26e4cf
MuhammadSaddique <saddiqueshah588@gmail.com>
2023-11-17 12:30:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const inputBox = document.getElementById("input");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+
+    }
+    inputBox.value = "";
+    saveData()
+}
+
+// listContainer.addEventListener("click",function(e){
+//     if(e.target.tagName === "li"){
+//         e.target.classList.toggle("Checked");
+//     }else if(e.target.tagName ==="span"){
+//             e.target.parentElement.remove();
+//      }
+// },false)
+
+
+
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function saveTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+saveTask();
+
+
